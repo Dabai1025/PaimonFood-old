78c314a1fc4d4e2fcab964efef8c4c8a67295bcf
Dabai1025 <65448593+Dabai1025@users.noreply.github.com>
2022-06-21 14:20:58 +0800

    Add files via upload

---
+++ src/main/java/com/ldsdb/paimonfood/tab/PaiMonFoodTab.java
@@ -0,0 +1,16 @@
+package com.ldsdb.paimonfood.tab;
+
+import com.ldsdb.paimonfood.item.RegItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class PaiMonFoodTab {
+    public static final String NAMETAB = "paimonfoodtab";
+
+    public static final CreativeModeTab ITEM_TAB = new CreativeModeTab(NAMETAB) {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(RegItem.PaiMonFoodItem.get());
+        }
+    };
+}
