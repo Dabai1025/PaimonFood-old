716d3502ca60787c738300b42f84622d3bfd9978
Funny Baymax <65448593+Dabai1025@users.noreply.github.com>
2022-07-06 23:56:59 +0800

    Add files via upload

--- src/main/java/com/ldsdb/paimonfood/item/RegItem.java
+++ src/main/java/com/ldsdb/paimonfood/item/RegItem.java
@@ -1,7 +1,6 @@
 package com.ldsdb.paimonfood.item;
 
 import com.ldsdb.paimonfood.Utils;
-import com.ldsdb.paimonfood.tool.PaimonSword;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,8 +9,5 @@ import net.minecraftforge.registries.RegistryObject;
 public class RegItem {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
     public static final RegistryObject<Item> PaiMonFoodItem = ITEMS.register("paimonfood_item", PaiMonFoodItem::new);
-    public static final DeferredRegister<Item> ITEMS_SWORD = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
-    public static final RegistryObject<Item> PaiMonSward = ITEMS_SWORD.register("paimon_sword", ()->{
-        return new PaimonSword();
-    });
 }
+
