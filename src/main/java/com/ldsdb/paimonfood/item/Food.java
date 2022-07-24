6aeac63fb0c35a3f429eaf83b75c13240d9438a1
Funny Baymax <65448593+Dabai1025@users.noreply.github.com>
2022-07-24 16:49:10 +0800

    削弱效果

--- src/main/java/com/ldsdb/paimonfood/item/Food.java
+++ src/main/java/com/ldsdb/paimonfood/item/Food.java
@@ -6,6 +6,6 @@ import net.minecraft.world.food.FoodProperties;
 
 public class Food {
     public static final FoodProperties FoodPaiMon = (new FoodProperties.Builder())
-            .nutrition(4).saturationMod(0.8F).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.SATURATION, 100, 2), 0.8F)
+            .nutrition(2).saturationMod(0.2F).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.SATURATION, 20, 1), 0.1F)
             .build();
 }
